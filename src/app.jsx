8b35f9e192e16453b220df3315cb1e576ccb6e75
Joao Gabriel <joaogabriel.farias@Hotmail.com>
2023-10-24 16:42:30 -0300

    ✅ Bateria 02 - Exercício 05

--- src/app.jsx
+++ src/app.jsx
@@ -1,72 +1,36 @@
-const skills = [
+const faqs = [
   {
     id: crypto.randomUUID(),
-    name: 'Cozinhar',
-    bgColor: 'bg-yellow',
-    emoji: '😋',
+    title: 'Quem é a maior celebridade do mundo dos games no BR?',
+    answer: 'A apresentadora Ana Maria Braga. Joga 3h pra desocupar a cabeça.',
   },
   {
     id: crypto.randomUUID(),
-    name: 'Programação',
-    bgColor: 'bg-red',
-    emoji: '👍',
+    title: 'Onde fica a europa da américa do sul?',
+    answer: 'Argentina. BR vive como rei em Buenos Aires.',
   },
-  { id: crypto.randomUUID(), name: 'Games', bgColor: 'bg-pink', emoji: '🤙' },
   {
     id: crypto.randomUUID(),
-    name: 'Inglês',
-    bgColor: 'bg-lightblue',
-    emoji: '🤟',
+    title: 'Qual é a grande oportunidade para desenvolvedores BR?',
+    answer: 'Aproveitar a alta demanda pra ganhar em dólar ou euro.',
   },
 ];
 
-const Avatar = () => (
-  <img
-    src="https://avatars.githubusercontent.com/u/119951373?v=4"
-    alt="User avatar"
-    className="avatar"
-  />
-);
-
-const Intro = () => (
-  <>
-    <h1>João Gabriel</h1>
-    <p>
-      Tenho 22 anos e moro em Curitiba. Comecei a entrar no mundo da programação
-      no meu emprego anterior, como trabalhava na área financeira, vi que muitos
-      processos e problemas do dia a dia poderiam ser resolvidos com python, e
-      desde então, fui conhecendo a área e me encontrei no javascript. No início
-      do ano, troquei a faculdade de economia por ADS, comecei a acompanhar os
-      vídeos do Roger e tenho me dedicado integralmente à fluência em
-      javascript, e como sinto que estou próximo, acho que chegou a hora do
-      react!
-    </p>
-  </>
-);
-
-const Skill = ({ name, emoji, bgColor }) => (
-  <li className={`${bgColor} skill`}>
-    {emoji} {name}
+const Item = ({ title, answer, i }) => (
+  <li className="item">
+    <p className="number">{i + 1}</p>
+    <h2 className="title">{title}</h2>
+    <p className="icon">-</p>
+    <div className="content-box">{answer}</div>
   </li>
 );
 
-const ListOfSkills = () => (
-  <ul className="list-of-skills">
-    {skills.map(({ id, name, emoji, bgColor }) => (
-      <Skill key={id} name={name} emoji={emoji} bgColor={bgColor} />
+const App = () => (
+  <ul className="accordion">
+    {faqs.map((faq, i) => (
+      <Item key={faq.id} title={faq.title} answer={faq.answer} i={i} />
     ))}
   </ul>
-);
-
-const App = () => (
-  <div className="card">
-    <Avatar />
-
-    <div className="info">
-      <Intro />
-      <ListOfSkills />
-    </div>
-  </div>
 );
 
 export { App };
