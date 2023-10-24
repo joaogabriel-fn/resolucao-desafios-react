9e9cb991b186b901b6679b7eabaf3c424afe970b
Joao Gabriel <joaogabriel.farias@Hotmail.com>
2023-10-24 17:29:51 -0300

    ✅ Bateria 02 - Exercício 06

--- src/app.jsx
+++ src/app.jsx
@@ -1,36 +1,145 @@
-const faqs = [
+const burgers = [
   {
     id: crypto.randomUUID(),
-    title: 'Quem é a maior celebridade do mundo dos games no BR?',
-    answer: 'A apresentadora Ana Maria Braga. Joga 3h pra desocupar a cabeça.',
+    name: 'Duo',
+    ingredients:
+      'Pão selado na manteiga, hambúrguer BOVINO de 160gr, cheddar, cebola caramelizada e molho especial',
+    price: 37,
+    photoName: 'img/burgers/duo.jpg',
+    soldOut: false,
   },
   {
     id: crypto.randomUUID(),
-    title: 'Onde fica a europa da américa do sul?',
-    answer: 'Argentina. BR vive como rei em Buenos Aires.',
+    name: 'Kids',
+    ingredients:
+      'Burger de blend de cogumelos e mandioca, abacaxi caramelizado, muçarela, alface, tomate e molho verde',
+    price: 29,
+    photoName: 'img/burgers/kids.jpg',
+    soldOut: false,
   },
   {
     id: crypto.randomUUID(),
-    title: 'Qual é a grande oportunidade para desenvolvedores BR?',
-    answer: 'Aproveitar a alta demanda pra ganhar em dólar ou euro.',
+    name: 'Master',
+    ingredients:
+      'Pão selado na manteiga, hambúrguer BOVINO de 100gr, bacon, cheddar e barbecue',
+    price: 51,
+    photoName: 'img/burgers/master.jpg',
+    soldOut: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Monster',
+    ingredients:
+      'Pão selado na manteiga, 2x hambúrguer BOVINO de 100gr, bacon, cheddar e barbecue',
+    price: 47,
+    photoName: 'img/burgers/monster.jpg',
+    soldOut: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Prime',
+    ingredients:
+      'Pão selado na manteiga, hambúrguer de CARNE DE SOL de 160gr, queijo coalho, bacon, alface, tomate, cebola roxa e melaço de cana',
+    price: 43,
+    photoName: 'img/burgers/prime.jpg',
+    soldOut: true,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Slim',
+    ingredients:
+      'Pão selado na manteiga, hambúrguer CUPIM de 180gr, 2 fatias de muçarela, alface, tomate, picles de cebola roxa e molho especial de alho',
+    price: 27,
+    photoName: 'img/burgers/slim.jpg',
+    soldOut: false,
   },
 ];
 
-const Item = ({ title, answer, i }) => (
-  <li className="item">
-    <p className="number">{i + 1}</p>
-    <h2 className="title">{title}</h2>
-    <p className="icon">-</p>
-    <div className="content-box">{answer}</div>
+const Header = () => (
+  <header>
+    <img className="logo" src="/img/logo-burgao-do-ze.jpg" alt="" />
+  </header>
+);
+
+const Burger = ({ price, name, ingredients, soldOut, img }) => (
+  <li className={`burger ${soldOut ? 'sold-out' : ''}`}>
+    <img src={img} alt={`Burger ${name} image`} />
+    <div>
+      <h3>{name}</h3>
+      <p>{ingredients}</p>
+      <span>{soldOut ? 'CABÔ' : `R$ ${price}`}</span>
+    </div>
   </li>
 );
 
+const Menu = () => (
+  <main className="menu">
+    {burgers.length > 0 && (
+      <>
+        <h2>Cardápio</h2>
+
+        <p>
+          Depois de uma semana desafiadora, nada melhor do que saborear um
+          burger onde cada mordida é um abraço saboroso, não é mesmo?🍔✨
+        </p>
+
+        <ul className="burgers">
+          {burgers.map((burger) => (
+            <Burger
+              key={burger.id}
+              price={burger.price}
+              name={burger.name}
+              ingredients={burger.ingredients}
+              soldOut={burger.soldOut}
+              img={burger.photoName}
+            />
+          ))}
+        </ul>
+      </>
+    )}
+  </main>
+);
+
+const OrderMsg = ({ closeHour }) => {
+  const handleClick = () => console.log('Direcionando para fazer pedido...');
+
+  return (
+    <div className="order">
+      <p>
+        🕛 Tamo aberto até as {closeHour}h. Vem visitar a gente ou faça seu
+        pedido online.
+      </p>
+      <button className="btn" onClick={handleClick}>
+        Fazer pedido
+      </button>
+    </div>
+  );
+};
+
+const Footer = () => {
+  const date = new Date();
+  const hour = date.getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour < closeHour;
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <OrderMsg closeHour={closeHour} />
+      ) : (
+        <p>🕛 Vamo abrir às {openHour}h</p>
+      )}
+    </footer>
+  );
+};
+
 const App = () => (
-  <ul className="accordion">
-    {faqs.map((faq, i) => (
-      <Item key={faq.id} title={faq.title} answer={faq.answer} i={i} />
-    ))}
-  </ul>
+  <div className="container">
+    <Header />
+    <Menu />
+    <Footer />
+  </div>
 );
 
 export { App };
