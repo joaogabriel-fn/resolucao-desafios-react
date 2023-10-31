f9556873bf5fa81b5cffdf4ba040eb9af0e40a6b
Joao Gabriel <joaogabriel.farias@Hotmail.com>
2023-10-30 21:19:37 -0300

    ✅ Bateria 03 - Exercício 02

--- src/app.jsx
+++ src/app.jsx
@@ -1,75 +1,51 @@
 import { useState } from 'react';
 
-const steps = [
-  {
-    id: crypto.randomUUID(),
-    description: 'Entender o problema do cliente',
-  },
-  {
-    id: crypto.randomUUID(),
-    description: 'Desenvolver a solução do problema',
-  },
-  {
-    id: crypto.randomUUID(),
-    description:
-      'Repetir até o cliente ficar feliz e encher seu 🍑 de dinheiro',
-  },
-];
-
-const Toggle = ({ shouldBeOpen, onClickToggle }) => (
-  <div className="container-toggle">
-    <button onClick={onClickToggle} className="toggle">
-      <span>{shouldBeOpen ? 'Fechar' : 'Abrir'}</span>
-    </button>
-  </div>
-);
-
-const Steps = ({ shouldBeOpen, step, onClickPrevious, onClickNext }) =>
-  shouldBeOpen && (
-    <div className="steps">
-      <ul className="numbers">
-        {steps.map((item, i) => (
-          <li key={item.id} className={i + 1 === step ? 'active' : ''}>
-            {i + 1}
-          </li>
-        ))}
-      </ul>
-
-      <p className="message">
-        Passo {step}: {steps[step - 1].description}
-      </p>
-
-      <div className="buttons">
-        <button onClick={onClickPrevious}>
-          <span>Anterior</span>
-        </button>
-        <button onClick={onClickNext}>
-          <span>Próximo</span>
-        </button>
-      </div>
-    </div>
-  );
-
-const App = () => {
-  const [shouldBeOpen, setShouldBeOpen] = useState(true);
-  const [step, setStep] = useState(1);
-
-  const handleClickToggle = () => setShouldBeOpen((s) => !s);
-  const handleClickPrevious = () => setStep((s) => (s - 1 === 0 ? s : s - 1));
-  const handleClickNext = () =>
-    setStep((s) => (s === steps.length ? s : s + 1));
+const DateMsg = ({ count }) => {
+  const date = new Date();
+  const formattedDate = new Intl.DateTimeFormat('pt-br', {
+    weekday: 'long',
+    month: 'short',
+    year: 'numeric',
+    day: '2-digit',
+  }).format(date.setDate(date.getDate() + count));
+  const singularPlural = count === 1 || count === -1 ? 'dia' : 'dias';
 
   return (
-    <>
-      <Toggle shouldBeOpen={shouldBeOpen} onClickToggle={handleClickToggle} />
+    <h2>
+      {count > 0
+        ? `${count} ${singularPlural} à partir de hoje será ${formattedDate}`
+        : count < 0
+        ? `${Math.abs(count)} ${singularPlural} atrás era ${formattedDate}}`
+        : `Hoje é ${formattedDate}`}
+    </h2>
+  );
+};
 
-      <Steps
-        shouldBeOpen={shouldBeOpen}
-        step={step}
-        onClickNext={handleClickNext}
-        onClickPrevious={handleClickPrevious}
-      />
-    </>
+const App = () => {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const decrementStep = () => setStep((s) => (s === 1 ? s : s - 1));
+  const decrementCount = () => setCount((c) => c - step);
+  const incrementStep = () => setStep((s) => s + 1);
+  const incrementCount = () => setCount((c) => c + step);
+
+  return (
+    <div className="container">
+      <div className="count">
+        <button onClick={decrementStep}>-</button>
+        <h2>Intervalo: {step}</h2>
+        <button onClick={incrementStep}>+</button>
+      </div>
+
+      <div className="count">
+        <button onClick={decrementCount}>-</button>
+        <h2>Contagem: {count}</h2>
+        <button onClick={incrementCount}>+</button>
+      </div>
+
+      <DateMsg count={count} />
+    </div>
   );
 };
 
